32120e9bee3f09904d43a7ff4400a96c59f5661b
Massimo <massimo.taddi02@gmail.com>
2023-09-20 14:35:31 +0200

    database

---
+++ Progetto-Libreria.sql
@@ -0,0 +1,65 @@
+create database Progetto_Libreria
+use Progetto_Libreria
+
+Create table Utenti
+(
+id INT PRIMARY KEY IDENTITY(1,1),
+nome VARCHAR(50),
+pass VARCHAR(64),
+ruolo VARCHAR(150)
+);
+Create table Libri
+(
+id INT PRIMARY KEY IDENTITY(1,1),
+titolo VARCHAR(255),
+autore VARCHAR(200),
+genere VARCHAR(100),
+quantita INT,
+formato BIT,
+nomeFile VARCHAR(255)
+);
+Create table Utenti_Libri
+(
+id INT PRIMARY KEY IDENTITY(1,1),
+idUtente INT,
+FOREIGN KEY (idUtente) REFERENCES Utenti(id)
+ON UPDATE SET NULL
+ON DELETE SET NULL,
+idLibri INT,
+FOREIGN KEY (idLibri) REFERENCES Libri(id)
+ON UPDATE SET NULL
+ON DELETE SET NULL,
+
+);
+
+
+
+INSERT INTO Utenti (nome, pass, ruolo)
+VALUES
+    ('Alice', HASHBYTES('Sha2_512','hashed_password_1'), 'Lettore'),
+    ('Bob',HASHBYTES('Sha2_512', 'hashed_password_2'), 'Amministratore'),
+    ('Charlie', HASHBYTES('Sha2_512','hashed_password_3'), 'Lettore');
+
+
+
+INSERT INTO Libri (titolo, autore, genere, quantita, formato, nomeFile)
+VALUES
+    ('Il Signore degli Anelli', 'J.R.R. Tolkien', 'Fantasy', 10, 0, 'signore_degli_anelli.pdf'),
+    ('1984', 'George Orwell', 'Distopia', 5, 1, '1984.epub'),
+    ('Harry Potter e la Pietra Filosofale', 'J.K. Rowling', 'Fantasy', 7, 0, 'harry_potter.pdf');
+
+INSERT INTO Utenti_Libri (idUtente, idLibri)
+VALUES
+    (1, 1),
+    (1, 2),
+    (2, 1),
+    (3, 3);
+
+
+select * from Utenti
+select * from Libri
+select * from Utenti_Libri
+
+drop table Utenti
+drop table Libri
+drop table Utenti_Libri
