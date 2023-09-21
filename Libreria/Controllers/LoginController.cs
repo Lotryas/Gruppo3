f9078939384599d1c9949c063cff510c9cd2c8ec
email <145462725+Dom-Visual@users.noreply.github.com>
2023-09-21 11:56:54 +0200

    Aggiunto Login

---
+++ Libreria/Controllers/LoginController.cs
@@ -0,0 +1,77 @@
+﻿using Libreria.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+
+namespace Libreria.Controllers
+{
+    public class LoginController : Controller
+    {
+        private ILogger<LoginController> ilogger;
+        private static Utente? utenteLoggato = null;
+        private static int chiamata = -1;
+
+        public LoginController(ILogger<LoginController> logger) 
+        { 
+            ilogger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            chiamata++;
+            ilogger.LogInformation($"TENTATIVO NUMERO:{chiamata}");
+            return View(chiamata);
+        }
+
+        public IActionResult Valida(Dictionary<string, string> keyValuePairs)
+        {
+           
+            if (DAOUtente.GetInstance().Validate(keyValuePairs["nome"], keyValuePairs["pass"]))
+            {
+
+                ilogger.LogInformation($"UTENTE LOGGATO: {keyValuePairs["user"]}");             
+                utenteLoggato = (Utente?)DAOUtente.GetInstance().Find(keyValuePairs["user"]);    
+                return View("Views/Login/Profilo.cshtml", utenteLoggato);                         
+            }
+            else
+            {
+                return Redirect("Index"); 
+            }
+
+        }
+
+        public IActionResult Logout()
+        {
+            
+            chiamata = -1;
+
+            ilogger.LogInformation($"LOGOUT: {utenteLoggato.Nome}");
+
+            utenteLoggato = null;
+
+            return Redirect("Index");
+
+        }
+
+        public IActionResult Registrazione()
+        {
+            return View();
+        }
+
+        public IActionResult Salva(Dictionary<string, object> keyValuePairs)
+        {
+            Utente utente = new Utente();
+            utente.PopulateFromRecord(keyValuePairs);
+            chiamata--;
+
+            if (DAOUtente.GetInstance().Insert(utente))
+            {
+                return Content("Registrazione avvenuta con successo");
+            }
+            else
+            {
+                return Content("Registrazione fallita");
+            }
+        }
+    }
+}
---
+++ Libreria/wwwroot/js/Login/ControlliLogin.js
@@ -0,0 +1,53 @@
+﻿function controlla_login()
+{
+    if (document.getElementById('user').value.length < 2) {
+        alert("IL NOME UTENTE " + document.getElementById('user').value + " CHE HAI INSERITO E' TROPPO BREVE");
+        return false;
+    }
+
+    if (document.getElementById('password_').value.length < 8) {
+        alert(" LA PASSWORD ISERITA E' TROPPO BREVE");
+        return false;
+    }
+
+}
+
+function controlli_registrazione()
+{
+    
+    if (document.getElementById('user').value.length < 2) {
+        alert("IL NOME UTENTE " + document.getElementById('user').value + " CHE HAI INSERITO E' TROPPO BREVE");
+        return false;
+    }
+
+    let p1 = document.getElementById('password1').value;
+    let p2 = document.getElementById('password2').value;
+
+    if (p1 != p2) {
+        alert("LE PASSWORD NON CORRISPONDONO");
+        return false;
+    }
+
+    let error = [];
+
+    if (p1.length < 8) {
+        error.push("La password deve avere almeno 8 caratteri");
+    }
+    if (p1.search(/[a-z]/) < 0) {
+        error.push("La password deve contenere almeno una lettera minuscola");
+    }
+    if (p1.search(/[A-Z]/) < 0) {
+        error.push("La password deve contenere almeno una lettera maiuscola");
+    }
+    if (p1.search(/[0-9]/) < 0) {
+        error.push("La password deve contenere almeno un numero");
+    }
+
+    if (error.length > 0) {
+        alert(error.join("\n"));
+        return false;
+    }
+
+    return true;
+
+}
