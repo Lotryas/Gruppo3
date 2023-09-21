1be51f6a2a47487222e450db963f68ce7b131954
FabioVenice <venice.fabio74@gmail.com>
2023-09-21 11:05:36 +0200

    Aggiunto DAOUtente_Libro

--- Libreria/Models/DAOUtente_Libro.cs
+++ Libreria/Models/DAOUtente_Libro.cs
@@ -1,4 +1,5 @@
-﻿using Utility;
+﻿using System.Data.SqlClient;
+using Utility;
 
 namespace Libreria.Models
 {
@@ -9,7 +10,7 @@ namespace Libreria.Models
 
         public static DAOUtente_Libro GetInstance()
         {
-            return _instance == null ? new() : _instance;
+            return _instance ??= new DAOUtente_Libro();
         }
 
         private DAOUtente_Libro()
@@ -23,22 +24,60 @@ namespace Libreria.Models
 
         public Entity? Find(long id)
         {
-            throw new NotImplementedException();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Utenti_Libri WHERE id = @id;");
+            cmd.Parameters.AddWithValue("@id", id);
+            Dictionary<string, object>? riga = _db?.ReadOne(cmd);
+            if (riga is null)
+                return null;
+            Utente_Libro ul = new();
+            ul.Id = (long)riga["id"];
+            ul.Utente = (Utente?)DAOUtente.GetInstance().Find((long)riga["idutente"]);
+            ul.Libro = (Libro?)DAOLibro.GetInstance().Find((long)riga["idlibro"]);
+            return ul;
         }
 
         public bool Insert(Entity entity)
         {
-            throw new NotImplementedException();
+            Utente_Libro ul = (Utente_Libro)entity;
+            SqlCommand cmd = new(@"
+                INSERT INTO Utenti_Libri (idUtente, idLibro)
+                VALUES (@idUtente, @idLibro);
+            ");
+            cmd.Parameters.AddWithValue("@idUtente", ul.Utente?.Id);
+            cmd.Parameters.AddWithValue("@idLibro", ul.Libro?.Id);
+            return _db.ExecQuery(cmd);
+
         }
 
         public List<Entity> ReadAll()
         {
-            throw new NotImplementedException();
+            List<Entity> ris = new();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Utenti_Libri");
+            List<Dictionary<string, object>> tabella = _db.ReadMany(cmd);
+            foreach (Dictionary<string,object> riga in tabella)
+            {
+                Utente_Libro ul = new();
+                ul.Id = (long)riga["id"];
+                ul.Utente = (Utente?)DAOUtente.GetInstance().Find((long)riga["idutente"]);
+                ul.Libro = (Libro?)DAOLibro.GetInstance().Find((long)riga["idlibro"]);
+                ris.Add(ul);
+            }
+            return ris;
         }
 
         public bool Update(Entity entity)
         {
-            throw new NotImplementedException();
+            Utente_Libro ul = (Utente_Libro)entity;
+            SqlCommand cmd = new(@"
+                UPDATE Utenti_Libri SET
+                    idUtente = @idUtente,
+                    idLibro = @idLibro
+                WHERE id = @id;
+            ");
+            cmd.Parameters.AddWithValue("@id", ul.Id);
+            cmd.Parameters.AddWithValue("@idUtente", ul.Utente?.Id);
+            cmd.Parameters.AddWithValue("@idLibro", ul.Libro?.Id);
+            return _db.ExecQuery(cmd);
         }
     }
 }
